09e8a8df6c9fef1a3e8277f22c923ec9667703e1
IL KYO OH <qdaszx@naver.com>
2022-03-21 08:00:00 +0900

    add FlatList practice

--- rnbasic/App.js
+++ rnbasic/App.js
@@ -1,17 +1,38 @@
 import React from 'react';
-import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
+import { Text, SafeAreaView, StyleSheet, FlatList } from 'react-native';
 import ColorBox from './components/ColorBox';
+
+const COLORS = [
+  { colorName: 'Base03', hexCode: '#002b36' },
+  { colorName: 'Base02', hexCode: '#073642' },
+  { colorName: 'Base01', hexCode: '#586e75' },
+  { colorName: 'Base00', hexCode: '#657b83' },
+  { colorName: 'Base0', hexCode: '#839496' },
+  { colorName: 'Base1', hexCode: '#93a1a1' },
+  { colorName: 'Base2', hexCode: '#eee8d5' },
+  { colorName: 'Base3', hexCode: '#fdf6e3' },
+  { colorName: 'Yellow', hexCode: '#b58900' },
+  { colorName: 'Orange', hexCode: '#cb4b16' },
+  { colorName: 'Red', hexCode: '#dc322f' },
+  { colorName: 'Magenta', hexCode: '#d33682' },
+  { colorName: 'Violet', hexCode: '#6c71c4' },
+  { colorName: 'Blue', hexCode: '#268bd2' },
+  { colorName: 'Cyan', hexCode: '#2aa198' },
+  { colorName: 'Green', hexCode: '#859900' },
+];
 
 const App = () => {
   return (
     <SafeAreaView>
-      <View style={styles.container}>
-        <Text style={styles.h1}>Here are some boxes of different colors</Text>
-        <ColorBox hexCode={'#2aa198'} colorName={'Cyan'} />
-        <ColorBox hexCode={'#268bd2'} colorName={'Blue'} />
-        <ColorBox hexCode={'#d33682'} colorName={'Megenta'} />
-        <ColorBox hexCode={'#cb4b16'} colorName={'Orange'} />
-      </View>
+      <FlatList
+        style={styles.container}
+        data={COLORS}
+        keyExtractor={(item) => item.hexCode}
+        renderItem={({ item }) => (
+          <ColorBox colorName={item.colorName} hexCode={item.hexCode} />
+        )}
+        ListHeaderComponent={<Text style={styles.h1}>Solarized</Text>}
+      />
     </SafeAreaView>
   );
 };
