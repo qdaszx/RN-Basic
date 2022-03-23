ef18a93957e36e29aee2a61af278720a21aca359
IL KYO OH <qdaszx@naver.com>
2022-03-24 08:00:00 +0900

    add network requests exercise

--- rnbasic/screens/Home.js
+++ rnbasic/screens/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import { FlatList } from 'react-native';
 import PalettePreview from '../components/PalettePreview';
 
@@ -44,9 +44,26 @@ const COLOR_PALETTES = [
 ];
 
 const Home = ({ navigation }) => {
+  const [colorPalette, setColorPalette] = useState([]);
+
+  const handleColorPalette = useCallback(async () => {
+    const result = await fetch(
+      'https://color-palette-api.kadikraman.vercel.app/palettes',
+    );
+    const palette = await result.json();
+
+    if (result.ok) {
+      setColorPalette(palette);
+    }
+  }, []);
+
+  useEffect(() => {
+    handleColorPalette();
+  }, []);
+
   return (
     <FlatList
-      data={COLOR_PALETTES}
+      data={colorPalette}
       keyExtractor={(item) => item.paletteName}
       renderItem={({ item }) => (
         <PalettePreview
